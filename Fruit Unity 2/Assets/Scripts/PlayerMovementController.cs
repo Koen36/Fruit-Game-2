ceb94000eabe7b53c269ef7dc1d1f89944763fa1
Koen <koenvdh7@gmail.com>
2022-06-07 12:29:35 +0200

    update

---
+++ Fruit Unity 2/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovementController : NetworkBehaviour
+{
+    public float Speed = 0.1f;
+    public GameObject PlayerModel;
+
+    public void Start()
+    {
+        PlayerModel.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Forest")
+        {
+            if(!PlayerModel.activeSelf)
+            {
+                SetPosition();
+                Debug.Log("Player spawned");
+
+                PlayerModel.SetActive(true);
+                Debug.Log("Forest PlayerModel enabled");
+            }
+
+            if (hasAuthority) //Only things that you have authority over and you want to control
+            {
+                Movement();
+            }
+        }
+    }
+
+    public void SetPosition() //Spawn player
+    {
+        transform.position = new Vector3(Random.Range(-10, 5), 1.5f, Random.Range(-10, 10)); //Random spawn player
+    }
+
+    public void Movement()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * Speed;
+    }
+}
